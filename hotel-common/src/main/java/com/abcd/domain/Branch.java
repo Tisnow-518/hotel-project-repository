9262196f0c679627006ce6b8528aeb6e9f5418f7
Qingjiu620 <2305454749@qq.com>
2025-07-14 23:07:17 +0800

    0

--- hotel-common/src/main/java/com/abcd/domain/Branch.java
+++ hotel-common/src/main/java/com/abcd/domain/Branch.java
@@ -1,12 +1,38 @@
 package com.abcd.domain;
 
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("branch")
 public class Branch {
 
+    @TableId
     private Integer branchId;
     private String branchName;
     private String branchAddress;
     private String branchPhone;
     private Integer roomCount;
-    private String branchPicUrl;
+    private String photoUrl;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill=FieldFill.UPDATE)
+    private Date updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createdBy;
+    @TableField(fill=FieldFill.UPDATE)
+    private String updatedBy;
+
+    @TableLogic
+    private int isDeleted;
 
 }
