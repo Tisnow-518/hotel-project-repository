828022ff40b9e934e21aece3b815de356a88c53b
Tisnow <835267308@qq.com>
2025-07-14 18:31:50 +0800

    hotel-service：   新增了hotel-service-auth模块   新增了hotel-service-branch模块   新增了hotel-service-room模块   可以在nacos上发现这三个服务 hotel-service-auth： hotel-service-branch： hotel-service-room：   新增了主函数   编写了一些配置   新增了一些依赖

---
+++ hotel-service/hotel-service-branch/src/main/java/com/abcd/branch/BranchMainApplication.java
@@ -0,0 +1,15 @@
+package com.abcd.branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchMainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BranchMainApplication.class, args);
+
+    }
+
+}
---
+++ hotel-service/hotel-service-room/src/main/java/com/abcd/room/RoomMainApplication.java
@@ -0,0 +1,15 @@
+package com.abcd.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RoomMainApplication.class, args);
+
+    }
+
+}
