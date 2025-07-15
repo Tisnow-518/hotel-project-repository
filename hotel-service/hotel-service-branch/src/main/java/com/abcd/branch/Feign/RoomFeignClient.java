521a5bf88f8fc65b4c02667b28a83a268a461cfa
Tisnow <835267308@qq.com>
2025-07-15 20:02:02 +0800

    修复了Feign接口方法的路径错误

--- hotel-service/hotel-service-branch/src/main/java/com/abcd/branch/Feign/RoomFeignClient.java
+++ hotel-service/hotel-service-branch/src/main/java/com/abcd/branch/Feign/RoomFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "hotel-service-room")
 public interface RoomFeignClient {
 
-    @GetMapping("/room/branchId/{branchId}")
+    @GetMapping("/api/room/branchId/{branchId}")
     ResponseResult getRoomListByBranchId(@PathVariable Integer branchId);
 
 }
