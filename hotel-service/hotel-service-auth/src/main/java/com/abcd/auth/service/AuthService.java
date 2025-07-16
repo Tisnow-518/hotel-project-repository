db14823b5dbf6b5a9695417283eb8c71d151ad46
Meteor539 <lifz2004@outlook.com>
2025-07-16 22:02:20 +0800

    修改了用户列表的加载功能，现在能够分页展示

--- hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/AuthService.java
+++ hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.abcd.auth.service;
 
+import com.abcd.hotel.domain.Room;
 import com.abcd.hotel.domain.User;
 import com.abcd.hotel.utils.ResponseResult;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface AuthService extends IService<User> {
@@ -10,5 +12,7 @@ public interface AuthService extends IService<User> {
      * 根据用户名获取用户
      */
     public User getUserByUserName(String userName);
+
+    Page<User> loadPagedUser(int pageNo, int pageSize);
 
 }
--- hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/impl/AuthServiceImpl.java
+++ hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/impl/AuthServiceImpl.java
@@ -1,9 +1,11 @@
 package com.abcd.auth.service.impl;
 
 import com.abcd.auth.service.AuthService;
+import com.abcd.hotel.domain.Room;
 import com.abcd.hotel.domain.User;
 import com.abcd.hotel.mapper.UserMapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,12 @@ public class AuthServiceImpl extends ServiceImpl<UserMapper, User> implements Au
                 .last("limit 1");
 
         return getOne(wrapper);
+    }
+
+    @Override
+    public Page<User> loadPagedUser(int pageNo, int pageSize) {
+        Page<User> page = new Page<>(pageNo, pageSize);
+        return this.page(page);
     }
 
 }
