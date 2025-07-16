6b31f91360722560248677a6045712639cf1c59b
Tisnow <835267308@qq.com>
2025-07-16 16:23:05 +0800

    实现了登录的接口方法
    不重要：
      添加Jwt依赖，实现Token

--- hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/AuthService.java
+++ hotel-service/hotel-service-auth/src/main/java/com/abcd/auth/service/AuthService.java
@@ -1,7 +1,14 @@
 package com.abcd.auth.service;
 
 import com.abcd.hotel.domain.User;
+import com.abcd.hotel.utils.ResponseResult;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface AuthService extends IService<User> {
+
+    /**
+     * 根据用户名获取用户
+     */
+    public User getUserByUserName(String userName);
+
 }
